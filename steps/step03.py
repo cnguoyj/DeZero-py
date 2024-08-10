9c0fa0da16883eb613c9e15e9c29de07e0769771
guoyanjun <guoyanjun.010@bytedance.com>
2024-08-10 19:36:23 +0800

    add step03

--- steps/step03.py
+++ steps/step03.py
@@ -7,7 +7,7 @@ class Variable:
 class Function:
     def __call__(self,input):
         x = input.data
-        y = x**2
+        y = self.forward(x)
         output = Variable(y)
         return output
 
@@ -17,4 +17,18 @@ class Function:
 class Square(Function):
     def forward(self, x):
         return x**2
+
+class Exp(Function):
+    def forward(self,x):
+        return np.exp(x)
     
+A = Square()
+B = Exp()
+C = Square()
+
+x = Variable(np.array(0.5))
+a = A(x)
+b = B(a)
+y = C(b)
+print(type(y))
+print(y.data)
